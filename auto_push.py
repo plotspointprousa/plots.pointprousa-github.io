4b29d68fb5f4dc7599027e92cdf057833a192ecb
sriramn1122 <ramsri28@outlook.com>
2024-07-30 07:25:18 -0400

    Auto commit on 2024-07-30 07:25:18

--- auto_push.py
+++ auto_push.py
@@ -38,6 +38,7 @@ if __name__ == "__main__":
     try:
         while True:
             time.sleep(10)
+            observer.join()
     except KeyboardInterrupt:
         observer.stop()
-    observer.join()
+    
